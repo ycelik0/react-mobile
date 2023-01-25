41695af9fe912e3994a11135fc0595e89722ee3d
Yusuf <yc5693410@gmail.com>
2023-01-25 16:07:58 +0100

    react native tutorial klaar

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {} from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeScreen} />
+        <Stack.Screen name="Image" component={ViewImageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ app/config/colors.js
@@ -0,0 +1,6 @@
+export default {
+  black: "#000",
+  white: "#fff",
+  primary: "#fc5c65",
+  secondary: "#4ecdc4",
+}
