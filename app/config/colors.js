9a8317e1fd689448f6466185b0e930b949194359
Yusuf <yc5693410@gmail.com>
2023-03-02 12:15:04 +0100

    navigation works with 3 screens. made loading screen. signin and signout not done yet

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,7 @@
 export default {
   black: "#000000",
   white: "#ffffff",
+  grey: "#839999",
   primary: "#F56D5C",
   secondary: "#0E3F3F",
 }
---
+++ app/screens/LoadingScreen.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+// Components
+import { SafeAreaView, View, Text, StyleSheet, Image } from "react-native";
+import * as Progress from "react-native-progress";
+// Assets
+import colors from "../config/colors";
+import logo from "../assets/images/orthofoodie_logo.png";
+
+function LoadingScreen({ navigation }) {
+  const [progressAmount, setProgressAmount] = useState(0);
+
+  if (progressAmount >= 1) {
+    navigation.navigate('SignIn')
+  }
+
+  return (
+    <SafeAreaView style={styles.outerContainer}>
+      <View>
+        <Image source={logo} style={styles.image} />
+        <Progress.Bar
+          progress={progressAmount}
+          width={78}
+          color={colors.primary}
+          borderColor={colors.primary}
+          style={styles.progressBar}
+        />
+        <Text style={styles.text}>Loading</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    color: colors.grey,
+    textAlign: "center",
+  },
+  image: {
+    alignSelf: "center",
+  },
+  progressBar: {
+    marginTop: 31,
+    marginBottom: 7,
+    alignSelf: "center",
+  },
+});
+
+export default LoadingScreen;
